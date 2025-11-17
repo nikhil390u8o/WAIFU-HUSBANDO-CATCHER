9d56262215cb1810cd1f55f5afbed60147a7f560
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-11-17 21:57:29 +0530

    Update inlinequery.py

--- shivu/modules/inlinequery.py
+++ shivu/modules/inlinequery.py
@@ -2,25 +2,15 @@ import re
 import time
 from html import escape
 from cachetools import TTLCache
-from pymongo import MongoClient, ASCENDING
 
 from telegram import Update, InlineQueryResultPhoto
-from telegram.ext import InlineQueryHandler, CallbackContext, CommandHandler 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import InlineQueryHandler, CallbackContext
 
-from shivu import user_collection, collection, application, db
+# In-memory storage instead of MongoDB
+all_characters_data = []  # List of all character dicts
+user_data_store = {}      # {user_id: {"id": ..., "first_name": ..., "characters": [...] }}
 
-
-# collection
-db.characters.create_index([('id', ASCENDING)])
-db.characters.create_index([('anime', ASCENDING)])
-db.characters.create_index([('img_url', ASCENDING)])
-
-# user_collection
-db.user_collection.create_index([('characters.id', ASCENDING)])
-db.user_collection.create_index([('characters.name', ASCENDING)])
-db.user_collection.create_index([('characters.img_url', ASCENDING)])
-
+# Caches
 all_characters_cache = TTLCache(maxsize=10000, ttl=36000)
 user_collection_cache = TTLCache(maxsize=10000, ttl=60)
 
@@ -34,14 +24,14 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
             if user_id in user_collection_cache:
                 user = user_collection_cache[user_id]
             else:
-                user = await user_collection.find_one({'id': int(user_id)})
+                user = user_data_store.get(int(user_id))
                 user_collection_cache[user_id] = user
 
             if user:
-                all_characters = list({v['id']:v for v in user['characters']}.values())
+                all_characters = list({v['id']: v for v in user['characters']}.values())
                 if search_terms:
                     regex = re.compile(' '.join(search_terms), re.IGNORECASE)
-                    all_characters = [character for character in all_characters if regex.search(character['name']) or regex.search(character['anime'])]
+                    all_characters = [c for c in all_characters if regex.search(c['name']) or regex.search(c['anime'])]
             else:
                 all_characters = []
         else:
@@ -49,12 +39,12 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
     else:
         if query:
             regex = re.compile(query, re.IGNORECASE)
-            all_characters = list(await collection.find({"$or": [{"name": regex}, {"anime": regex}]}).to_list(length=None))
+            all_characters = [c for c in all_characters_data if regex.search(c['name']) or regex.search(c['anime'])]
         else:
             if 'all_characters' in all_characters_cache:
                 all_characters = all_characters_cache['all_characters']
             else:
-                all_characters = list(await collection.find({}).to_list(length=None))
+                all_characters = all_characters_data
                 all_characters_cache['all_characters'] = all_characters
 
     characters = all_characters[offset:offset+50]
@@ -66,15 +56,24 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
 
     results = []
     for character in characters:
-        global_count = await user_collection.count_documents({'characters.id': character['id']})
-        anime_characters = await collection.count_documents({'anime': character['anime']})
+        global_count = sum(character['id'] in [c['id'] for u in user_data_store.values() for c in u['characters']])
+        anime_characters = sum(c['anime'] == character['anime'] for c in all_characters_data)
 
-        if query.startswith('collection.'):
+        if query.startswith('collection.') and user_id.isdigit() and user:
             user_character_count = sum(c['id'] == character['id'] for c in user['characters'])
             user_anime_characters = sum(c['anime'] == character['anime'] for c in user['characters'])
-            caption = f"<b> Look At <a href='tg://user?id={user['id']}'>{(escape(user.get('first_name', user['id'])))}</a>'s Character</b>\n\n🌸: <b>{character['name']} (x{user_character_count})</b>\n🏖️: <b>{character['anime']} ({user_anime_characters}/{anime_characters})</b>\n<b>{character['rarity']}</b>\n\n<b>🆔️:</b> {character['id']}"
+            caption = f"<b>Look At <a href='tg://user?id={user['id']}'>{escape(user.get('first_name', user['id']))}</a>'s Character</b>\n\n" \
+                      f"🌸: <b>{character['name']} (x{user_character_count})</b>\n" \
+                      f"🏖️: <b>{character['anime']} ({user_anime_characters}/{anime_characters})</b>\n" \
+                      f"<b>{character['rarity']}</b>\n\n<b>🆔️:</b> {character['id']}"
         else:
-            caption = f"<b>Look At This Character !!</b>\n\n🌸:<b> {character['name']}</b>\n🏖️: <b>{character['anime']}</b>\n<b>{character['rarity']}</b>\n🆔️: <b>{character['id']}</b>\n\n<b>Globally Guessed {global_count} Times...</b>"
+            caption = f"<b>Look At This Character !!</b>\n\n" \
+                      f"🌸:<b> {character['name']}</b>\n" \
+                      f"🏖️: <b>{character['anime']}</b>\n" \
+                      f"<b>{character['rarity']}</b>\n" \
+                      f"🆔️: <b>{character['id']}</b>\n\n" \
+                      f"<b>Globally Guessed {global_count} Times...</b>"
+
         results.append(
             InlineQueryResultPhoto(
                 thumbnail_url=character['img_url'],
@@ -87,4 +86,5 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
 
     await update.inline_query.answer(results, next_offset=next_offset, cache_time=5)
 
+# Add handler to your application
 application.add_handler(InlineQueryHandler(inlinequery, block=False))
