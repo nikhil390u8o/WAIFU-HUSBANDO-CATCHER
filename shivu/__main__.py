16b59c327bea01f14ae8ce83e119dc230a14551b
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-11-17 22:38:23 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -11,7 +11,7 @@ from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
 
 from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
-from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, db, LOGGER
+from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, LOGGER
 from shivu.modules import ALL_MODULES
 
 
