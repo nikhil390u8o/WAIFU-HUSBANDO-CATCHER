97291131c1c81f7cbf6d4521a7a89017700f5c99
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-11-17 22:03:26 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -1,8 +1,6 @@
-import logging  
-import os
-from pyrogram import Client 
+import logging
+from pyrogram import Client
 from telegram.ext import Application
-from motor.motor_asyncio import AsyncIOMotorClient
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -17,27 +15,26 @@ LOGGER = logging.getLogger(__name__)
 
 from shivu.config import Development as Config
 
-
 api_id = Config.api_id
 api_hash = Config.api_hash
 TOKEN = Config.TOKEN
 GROUP_ID = Config.GROUP_ID
-CHARA_CHANNEL_ID = Config.CHARA_CHANNEL_ID 
-mongo_url = Config.mongo_url 
-PHOTO_URL = Config.PHOTO_URL 
-SUPPORT_CHAT = Config.SUPPORT_CHAT 
+CHARA_CHANNEL_ID = Config.CHARA_CHANNEL_ID
+PHOTO_URL = Config.PHOTO_URL
+SUPPORT_CHAT = Config.SUPPORT_CHAT
 UPDATE_CHAT = Config.UPDATE_CHAT
-BOT_USERNAME = Config.BOT_USERNAME 
+BOT_USERNAME = Config.BOT_USERNAME
 sudo_users = Config.sudo_users
-OWNER_ID = Config.OWNER_ID 
+OWNER_ID = Config.OWNER_ID
 
+# Telegram Bot applications
 application = Application.builder().token(TOKEN).build()
 shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
-lol = AsyncIOMotorClient(mongo_url)
-db = lol['Character_catcher']
-collection = db['anime_characters_lol']
-user_totals_collection = db['user_totals_lmaoooo']
-user_collection = db["user_collection_lmaoooo"]
-group_user_totals_collection = db['group_user_totalsssssss']
-top_global_groups_collection = db['top_global_groups']
-pm_users = db['total_pm_users']
+
+# ----- In-memory replacements for MongoDB collections -----
+collection = {}  # anime characters {id: character_dict}
+user_totals_collection = {}  # {chat_id: {"message_frequency": int, ...}}
+user_collection = {}  # {user_id: {"characters": [character_dicts], ...}}
+group_user_totals_collection = {}  # {group_id: {user_id: total}}
+top_global_groups_collection = {}  # {group_id: total_guesses}
+pm_users = {}  # {user_id: {"first_name": str, "username": str}}
